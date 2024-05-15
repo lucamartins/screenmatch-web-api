dba5595d7a0576de28f841725e11ca0b9cf478f4
Luca Santos Martins <lucamartins.dev@gmail.com>
2024-05-15 13:12:42 -0300

    add SerieDTO

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,13 +1,28 @@
 package br.com.alura.screenmatch.controller;
 
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class SerieController {
+    @Autowired
+    private SerieRepository serieRepository;
+
     @GetMapping("/series")
-    String getAllSeries() {
-        return "Here it will be listed all series.";
+    List<SerieDTO> getAllSeries() {
+        var series = serieRepository
+                .findAll()
+                .stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(),
+                        s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .toList();
+
+        return series;
     }
 
 }
