5750e8ec6bb08d84ee755160f49dfce79af85f1b
Luca Santos Martins <lucamartins.dev@gmail.com>
2024-05-15 16:58:28 -0300

    add get top 5 series

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -5,17 +5,24 @@ import br.com.alura.screenmatch.repository.SerieRepository;
 import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
     @Autowired
     private SerieService service;
 
-    @GetMapping("/series")
+    @GetMapping
     List<SerieDTO> getAllSeries() {
         return service.getAllSeries();
+    }
+
+    @GetMapping("/top5")
+    List<SerieDTO> getTop5Series() {
+        return service.getTop5Series();
     }
 }
--- src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -1,6 +1,7 @@
 package br.com.alura.screenmatch.service;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,13 +13,21 @@ public class SerieService {
     @Autowired
     private SerieRepository serieRepository;
 
-    public List<SerieDTO> getAllSeries() {
-        return serieRepository
-                .findAll()
+    private List<SerieDTO> convertSeriesToDTO(List<Serie> series) {
+        return series
                 .stream()
                 .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(),
                         s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
                 .toList();
+    }
 
+    public List<SerieDTO> getAllSeries() {
+        return convertSeriesToDTO(serieRepository.findAll());
+
+
+    }
+
+    public List<SerieDTO> getTop5Series() {
+        return convertSeriesToDTO(serieRepository.findTop5ByOrderByAvaliacaoDesc());
     }
 }
